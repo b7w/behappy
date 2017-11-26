51c2ec79bbbb81d435a7452408392bee215510d4
b7w@isudo.ru <b7w@isudo.ru>
2017-11-26 22:06:46 +0300

    Add year filter

--- src/behappy/main.py
+++ src/behappy/main.py
@@ -29,12 +29,6 @@ class Settings:
 
     def image_sizes(self):
         return {
-            'tiny': {
-                'WIDTH': 150,
-                'HEIGHT': 150,
-                'CROP': True,
-                'QUALITY': 85,
-            },
             'small': {
                 'WIDTH': 300,
                 'HEIGHT': 300,
@@ -43,10 +37,6 @@ class Settings:
             'big': {
                 'WIDTH': 1920,
                 'HEIGHT': 1080,
-            },
-            'full': {
-                'WIDTH': 10 ** 6,
-                'HEIGHT': 10 ** 6,
             },
         }
 
@@ -88,6 +78,9 @@ class Gallery:
         self.desc = desc
         self.albums = []
 
+    def years(self):
+        return list(reversed(list(set(i.date.year for i in self.albums))))
+
 
 class Image:
     def __init__(self, path):
@@ -125,8 +118,8 @@ class ImageSet:
         """
         self.path = path
         self.thumbnail_path = thumbnail
-        self.include = [i.strip() for i in include.split(',')] or []
-        self.exclude = [i.strip() for i in exclude.split(',')] or []
+        self.include = [i.strip() for i in include.split(',') if i.strip()] or []
+        self.exclude = [i.strip() for i in exclude.split(',') if i.strip()] or []
 
     def images(self, all=False):
         result = set()
@@ -194,6 +187,7 @@ class BeHappy:
         self.copy_static_resources()
         self.render_about_page()
         self.render_gallery_pages()
+        self.render_gallery_per_year_pages()
         self.render_album_pages()
 
     def render_about_page(self):
@@ -203,17 +197,32 @@ class BeHappy:
             f.write(html)
 
     def render_gallery_pages(self):
-        params = dict(title='Welcome', description=self.gallery.desc, albums=self.gallery.albums)
+        params = dict(title='Welcome', description=self.gallery.desc, albums=self.gallery.albums,
+                      years=self.gallery.years())
         html = self.jinja.get_template('gallery.jinja2').render(**params,
                                                                 **settings.templates_parameters())
         with open('./target/index.html', mode='w') as f:
             f.write(html)
 
+    def render_gallery_per_year_pages(self):
+        groped = {}
+        for album in self.gallery.albums:
+            groped.setdefault(album.date.year, []).append(album)
+        for year, albums in groped.items():
+            params = dict(title='Welcome', description=self.gallery.desc, albums=albums, years=self.gallery.years(),
+                          current_year=year)
+            html = self.jinja.get_template('gallery.jinja2').render(**params,
+                                                                    **settings.templates_parameters())
+            folder = Path('./target/year/{}'.format(year))
+            folder.mkdir(parents=True, exist_ok=True)
+            with open(Path(folder, 'index.html').as_posix(), mode='w') as f:
+                f.write(html)
+
     def render_album_pages(self):
         for album in self.gallery.albums:
             params = dict(album=album, images=album.image_set.images())
             html = self.jinja.get_template('album.jinja2').render(**params,
-                                                                **settings.templates_parameters())
+                                                                  **settings.templates_parameters())
             with open('./target/album/{}/index.html'.format(album.id), mode='w') as f:
                 f.write(html)
 
