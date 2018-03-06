fbc324ce40588542bbd5b91891ed9630deedbfc4
b7w@isudo.ru <b7w@isudo.ru>
2018-03-06 23:30:32 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='behappy',
+    version='0.1',
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if i.strip()],
+    packages=['behappy', 'behappy.core', ],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'behappy = behappy.cli:main',
+        ],
+    },
+    url='https://bitbucket.org/b7w/behappy',
+    license='MIT',
+    author='B7W',
+    author_email='b7w@isudo.ru',
+    description=open('readme.md').read(),
+)
