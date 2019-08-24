4124b9d0efdb546a6e8618ad76417febf7bd89e7
b7w@isudo.ru <b7w@isudo.ru>
2019-08-24 12:03:17 +0300

    Add album hidden behavior

--- src/behappy/core/main.py
+++ src/behappy/core/main.py
@@ -272,6 +272,7 @@ class BeHappy:
         albums_count = len(self.gallery.albums())
         image_count = sum(len(i.image_set.images(all=True)) for i in self.gallery.albums())
         print('Load {} albums and {} images'.format(albums_count, image_count))
-        print('Find {} hidden albums:'.format(len(self.gallery.top_hidden_albums())))
-        for album in self.gallery.top_hidden_albums():
-            print('\t{} [{}] {} images'.format(album.id, album.title, len(album.image_set.images())))
+        if self.gallery.top_hidden_albums():
+            print('Find {} hidden albums:'.format(len(self.gallery.top_hidden_albums())))
+            for album in self.gallery.top_hidden_albums():
+                print('\t{} [{}] {} images'.format(album.id, album.title, len(album.image_set.images())))
