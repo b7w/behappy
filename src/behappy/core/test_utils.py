ef50c5bdd7fa44ace29def992e7ec09a3b83f3e4
b7w@isudo.ru <b7w@isudo.ru>
2019-09-22 00:07:00 +0300

    Add tests for exif

--- src/behappy/core/test_utils.py
+++ src/behappy/core/test_utils.py
@@ -1,9 +1,19 @@
+from datetime import datetime
+from pathlib import Path
 from unittest import TestCase
+from urllib.request import urlretrieve
 
-from behappy.core.utils import parse_orientation
+from behappy.core.utils import parse_orientation, read_exif
 
 
 class TestUtils(TestCase):
+
+    def setUp(self):
+        self.test_img1 = Path('tmp/image-01.jpg')
+        if not self.test_img1.exists():
+            url = 'https://s3-eu-west-1.amazonaws.com/b7w.distributions/behappy/tests/image-01.jpg'
+            self.test_img1.parent.mkdir(exist_ok=True)
+            urlretrieve(url, filename=img.as_posix())
 
     def test_parse_orientation(self):
         self.assertEqual(parse_orientation(None), 0)
@@ -15,3 +25,22 @@ class TestUtils(TestCase):
         self.assertEqual(parse_orientation('Rotate 90 CW'), 270)
         self.assertEqual(parse_orientation('Mirror horizontal and rotate 90 CW'), 0)
         self.assertEqual(parse_orientation('Rotate 270 CW'), 90)
+
+    def test_exif_properties(self):
+        _, exif = read_exif([Path(self.test_img1)])[0]
+
+        self.assertEqual(exif.maker, 'Fujifilm')
+        self.assertEqual(exif.model, 'X-T30')
+        self.assertEqual(exif.lens_model, 'XF35mmF2 R WR')
+        self.assertEqual(exif.iso, 320)
+        self.assertEqual(exif.fnumber, 2.0)
+        self.assertEqual(exif.exposure_time, '1/420')
+        self.assertEqual(exif.focal_length, '35.0 mm')
+        self.assertEqual(exif.orientation, 0)
+        self.assertEqual(exif.datetime_original, datetime(2019, 8, 21, 18, 49, 49))
+        self.assertEqual(exif.style, 'Astia')
+
+    def test_exif_info(self):
+        _, exif = read_exif([Path(self.test_img1)])[0]
+
+        self.assertEqual(exif.info(), 'Fujifilm X-T30  XF35mmF2 R WR | ISO320  f/2.0  1/420s | Astia')
